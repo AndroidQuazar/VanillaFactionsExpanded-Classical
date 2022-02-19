608797a2a8696cb4366fe3625fdb93153c9b2ab5
Joshua <legodude17@users.noreply.github.com>
2022-02-19 15:45:15 -0800

    Tents

--- 1.3/Source/VFEC/VFEC/ClassicMod.cs
+++ 1.3/Source/VFEC/VFEC/ClassicMod.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using RimWorld;
 using Verse;
 
 namespace VFEC
@@ -7,6 +8,17 @@ namespace VFEC
     {
         public static Harmony Harm;
 
-        public ClassicMod(ModContentPack content) : base(content) => Harm = new Harmony("oskarpotocki.vfe.classical");
+        public ClassicMod(ModContentPack content) : base(content)
+        {
+            Harm = new Harmony("oskarpotocki.vfe.classical");
+            Harm.Patch(AccessTools.Method(typeof(Toils_LayDown), "ApplyBedThoughts"), postfix: new HarmonyMethod(GetType(), nameof(TellBedAboutThoughts)));
+        }
+
+        public static void TellBedAboutThoughts(Pawn actor)
+        {
+            if (actor.CurrentBed() is {AllComps: var comps})
+                foreach (var comp in comps)
+                    comp?.Notify_AddBedThoughts(actor);
+        }
     }
 }
---
+++ 1.3/Source/VFEC/VFEC/Comps/CompTent.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VFEC.Comps
+{
+    public class CompTent : ThingComp
+    {
+        public override void Notify_AddBedThoughts(Pawn pawn)
+        {
+            base.Notify_AddBedThoughts(pawn);
+            pawn.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptOutside);
+            pawn.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptOnGround);
+            pawn.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInCold);
+            pawn.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInHeat);
+        }
+    }
+}
