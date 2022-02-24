3c8705e986edc68c6bb93f956df71a49d5ef4595
Joshua <legodude17@users.noreply.github.com>
2022-02-24 09:37:44 -0800

    Javelin

---
+++ 1.3/Source/VFEC/VFEC/DamageWorker_ApparelDamage.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Verse;
+
+namespace VFEC
+{
+    public class DamageWorker_ApparelDamage : DamageWorker
+    {
+        public override DamageResult Apply(DamageInfo dinfo, Thing victim)
+        {
+            if (victim is Pawn {apparel: var apparelTracker} && apparelTracker.WornApparel.Where(t => t.def.useHitPoints).TryRandomElement(out var apparel))
+                return apparel.TakeDamage(dinfo);
+            return new DamageResult();
+        }
+    }
+}
